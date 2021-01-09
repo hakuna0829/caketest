a779876692d8fd7c276660aeba65821e66fd5ab6
unknown <hakuna0829@gmail.com>
2021-01-10 01:56:36 +0700

    add test apis

---
+++ src/Controller/ContactsController.php
@@ -0,0 +1,132 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Contacts Controller
+ *
+ * @property \App\Model\Table\ContactsTable $Contacts
+ *
+ * @method \App\Model\Entity\Contact[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ContactsController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+        $this->set('contacts', $this->paginate());
+        // Specify which view vars JsonView should serialize.
+        $this->set('_serialize', 'contacts');
+    }
+
+    public function indexExt()
+    {
+        $this->paginate = ['contain' => ['Companies']];
+        // $contacts = $this->paginate($this->Contacts);
+
+        $this->set('contacts', $this->paginate());
+        // Specify which view vars JsonView should serialize.
+        $this->set('_serialize', 'contacts');
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Contact id.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $contact = $this->Contacts->get($id, [
+            'contain' => ['Companies'],
+        ]);
+
+        $this->set('contact', $contact);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $res = array();
+        $contact = $this->Contacts->newEntity();
+        if ($this->request->is('post')) {
+            $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
+            $lastId = $this->Contacts->find()->select(['id'])->last()['id'] + 1 ;
+
+            $contact['id'] = $lastId;
+            if ($this->Contacts->save($contact)) {
+                $res['status'] = true;
+                $res['msg'] = ('The new contact has been saved.');
+            } else {
+                $res['status'] = false;
+                $res['msg'] = ('The contact could not be saved. Please, try again.');
+            }
+        }
+        $companies = $this->Contacts->Companies->find('list', ['limit' => 200]);
+        $this->set(compact('contact', 'companies', 'res'));
+        
+        $this->set('_serialize', ['contacts', 'res']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Contact id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $contact = $this->Contacts->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
+            if ($this->Contacts->save($contact)) {
+                $this->Flash->success(__('The contact has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The contact could not be saved. Please, try again.'));
+        }
+        $companies = $this->Contacts->Companies->find('list', ['limit' => 200]);
+        $this->set(compact('contact', 'companies'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Contact id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $contact = $this->Contacts->get($id);
+        if ($this->Contacts->delete($contact)) {
+            $this->Flash->success(__('The contact has been deleted.'));
+        } else {
+            $this->Flash->error(__('The contact could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
